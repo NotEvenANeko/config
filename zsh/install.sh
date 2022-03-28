e8f4d0e2545b424d0da16ca67354acb156ce05de
Neko <neko@qwq.icu>
2022-03-29 00:45:44 +0800

    🐞 fix(install.sh): install p10k via oh-my-zsh instead of manual

--- zsh/install.sh
+++ zsh/install.sh
@@ -30,8 +30,7 @@ curl -o $HOME/.p10k.zsh https://raw.githubusercontent.com/Cattttttttt/config/mas
 
 curl -o $HOME/.vimrc https://raw.githubusercontent.com/Cattttttttt/config/master/vim/.vimrc
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 chsh -s $(which zsh)
 
