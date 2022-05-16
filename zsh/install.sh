33b4b2158fcf1f5a2f927c1df25fe03c3a4bc9e8
Neko <neko@qwq.icu>
2022-05-16 18:55:45 +0800

    ✨ feat(install.sh): add unattended for bootstrap use

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,17 +7,27 @@ isPacman=$?
 sudo --version > /dev/null 2> /dev/null
 isSudo=$?
 
-if [ "$isSudo" -ne 0 ]; then
-  echo "you need sudo to run this script."
-  exit 1
-fi
+for arg in "$@"
+do
+  case "$arg" in
+    "--unattended")
+      isUnattended=1
+    ;;
+  esac
+done
 
+if [ -z "$isUnattended" ]; then
+  if [ "$isSudo" -ne 0 ]; then
+    echo "you need sudo to run this script."
+    exit 1
+  fi
 
-if [ "$isApt" -eq 0 ]; then
-  sudo apt update && sudo apt upgrade -y
-  sudo apt install zsh git curl wget neofetch thefuck vim -y
-elif [ "$isPacman" -eq 0 ]; then
-  sudo pacman -Sy --noconfirm --needed which base zsh git curl wget neofetch thefuck vim
+  if [ "$isApt" -eq 0 ]; then
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install zsh git curl wget neofetch thefuck vim -y
+  elif [ "$isPacman" -eq 0 ]; then
+    sudo pacman -Sy --noconfirm --needed which base zsh git curl wget neofetch thefuck vim
+  fi
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -26,14 +36,16 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-curl -o $HOME/.zshrc https://raw.githubusercontent.com/Cattttttttt/config/master/zsh/.zshrc
+curl -so $HOME/.zshrc https://raw.githubusercontent.com/Cattttttttt/config/master/zsh/.zshrc
 
-curl -o $HOME/.p10k.zsh https://raw.githubusercontent.com/Cattttttttt/config/master/zsh/.p10k.zsh
+curl -so $HOME/.p10k.zsh https://raw.githubusercontent.com/Cattttttttt/config/master/zsh/.p10k.zsh
 
-curl -o $HOME/.vimrc https://raw.githubusercontent.com/Cattttttttt/config/master/vim/.vimrc
+curl -so $HOME/.vimrc https://raw.githubusercontent.com/Cattttttttt/config/master/vim/.vimrc
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 chsh -s $(which zsh)
 
-zsh
+if [ -z "$isUnattended" ]; then
+  zsh
+fi
