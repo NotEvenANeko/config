32b42d2141a6c1b825d33430befd8b2f5020626f
Neko <neko@qwq.icu>
2023-08-02 22:38:04 +0800

    fix(eslint): fix next config conflict

--- eslint/src/next.ts
+++ eslint/src/next.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'eslint-define-config';
 
+import '@rushstack/eslint-patch/modern-module-resolution';
+
 export = defineConfig({
-  extends: [require.resolve('./react'), 'next/core-web-vitals'],
+  extends: ['plugin:react/jsx-runtime', 'next/core-web-vitals'],
 });
